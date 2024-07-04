9d9d94eb8ac5ad16d5f7d72d580b6fb5a56f8ace
Naushad101 <naushad.shaikh@bnt-soft.com>
2024-07-04 23:30:27 +0530

    Add Logger to Subcategory Controller

--- src/main/java/com/example/controller/SubCategoryController.java
+++ src/main/java/com/example/controller/SubCategoryController.java
@@ -2,7 +2,8 @@ package com.example.controller;
 
 import java.util.List;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -17,49 +18,59 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.exception.CategoryNotFoundException;
 import com.example.exception.SubCategoryIsAlreadyPresent;
 import com.example.exception.SubCategoryNotFoundException;
-import com.example.model.Category;
 import com.example.model.SubCategory;
 import com.example.service.SubCategoryService;
 
+import lombok.extern.slf4j.Slf4j;
+
 @RestController
-@RequestMapping("/api/subcategories")
+@RequestMapping("/api/subcategory")
+@Slf4j
 public class SubCategoryController {
 
-    
-    private SubCategoryService subCategoryService;
+   
+    private final SubCategoryService subCategoryService;
 
     public SubCategoryController(SubCategoryService subCategoryService){
-        this.subCategoryService=subCategoryService;
+        this.subCategoryService = subCategoryService;
     }
-    
     
     @PostMapping()
-    public ResponseEntity<SubCategory> saveSubCategory(@RequestBody SubCategory subCategory) throws SubCategoryIsAlreadyPresent,CategoryNotFoundException{
-       return subCategoryService.saveSubCategory(subCategory);
+    public ResponseEntity<SubCategory> saveSubCategory(@RequestBody SubCategory subCategory) throws SubCategoryIsAlreadyPresent, CategoryNotFoundException {
+        log.info("Saving subcategory: {}", subCategory.getSubCategoryName());
+        ResponseEntity<SubCategory> responseEntity = subCategoryService.saveSubCategory(subCategory);
+        log.info("Saved subcategory: {}", responseEntity.getBody().getSubCategoryName());
+        return responseEntity;
     }
-    
     
     @GetMapping()
     public ResponseEntity<List<SubCategory>> getAllSubCategories() throws SubCategoryNotFoundException {
+        log.info("Fetching all subcategories");
         List<SubCategory> subCategories = subCategoryService.getAllSubCategories();
+        log.info("Fetched {} subcategories", subCategories.size());
         return new ResponseEntity<>(subCategories, HttpStatus.OK);
     }
     
-    
     @GetMapping("/{id}")
-    public ResponseEntity<SubCategory> getSubCategoryById(@PathVariable Long id) throws SubCategoryNotFoundException{
-       return subCategoryService.getSubCategoryById(id);
+    public ResponseEntity<SubCategory> getSubCategoryById(@PathVariable Long id) throws SubCategoryNotFoundException {
+        log.info("Fetching subcategory with id: {}", id);
+        ResponseEntity<SubCategory> responseEntity = subCategoryService.getSubCategoryById(id);
+        log.info("Fetched subcategory with id {}: {}", id, responseEntity.getBody().getSubCategoryName());
+        return responseEntity;
     }
     
     @PutMapping("/{id}")
     public ResponseEntity<SubCategory> updateSubCategory(@PathVariable Long id, @RequestBody SubCategory subCategory) throws SubCategoryNotFoundException {
-       return subCategoryService.updateSubCategory(id, subCategory);
+        log.info("Updating subcategory with id: {}", id);
+        ResponseEntity<SubCategory> responseEntity = subCategoryService.updateSubCategory(id, subCategory);
+        log.info("Updated subcategory with id {}: {}", id, responseEntity.getBody().getSubCategoryName());
+        return responseEntity;
     }
     
-    // Delete operation
     @DeleteMapping("/{id}")
     public void deleteSubCategory(@PathVariable Long id) throws SubCategoryNotFoundException {
-       subCategoryService.deleteSubCategory(id);
+        log.info("Deleting subcategory with id: {}", id);
+        subCategoryService.deleteSubCategory(id);
+        log.info("Deleted subcategory with id: {}", id);
     }
 }
-
