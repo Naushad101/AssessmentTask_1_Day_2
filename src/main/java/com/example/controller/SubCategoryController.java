51eca8029f8b221f03b55981ce392212855fea74
Naushad101 <naushad.shaikh@bnt-soft.com>
2024-07-05 09:42:13 +0530

    Added RequestMapping

--- src/main/java/com/example/controller/SubCategoryController.java
+++ src/main/java/com/example/controller/SubCategoryController.java
@@ -22,7 +22,7 @@ import com.example.service.SubCategoryService;
 import lombok.extern.slf4j.Slf4j;
 
 @RestController
-@RequestMapping("/api/subcategory")
+@RequestMapping("/subcategory")
 @Slf4j
 public class SubCategoryController {
 
